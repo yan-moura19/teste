a78e2c54a1469da0b95ac1ed9a0729c6bd58133c
Yan Gabriel <43892743+yan-moura19@users.noreply.github.com>
2022-02-05 01:53:01 -0300

    Delete backend/backend directory

--- backend/backend/src/main/java/com/br/api/backend/Repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.api.backend.Repository;
-
-import com.br.api.backend.Models.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoRepository extends JpaRepository<Pedido,Long> {
-}
